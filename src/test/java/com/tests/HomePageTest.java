67fa380d6389b628b2d1db27ddc9de4e60ea9123
vaibhav <vvaidya6@gmail.com>
2022-05-05 10:52:22 +0530

    Initial commit

---
+++ src/test/java/com/tests/HomePageTest.java
@@ -0,0 +1,11 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+
+    @Test
+    public void homeTest(){
+        System.out.println("Home Page Test");
+    }
+}
---
+++ src/test/java/com/tests/LoginTest.java
@@ -0,0 +1,11 @@
+package com.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void test(){
+        System.out.println("Testing");
+    }
+}
